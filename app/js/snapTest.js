9613a69c8ed92781ad43f3e9950cb0b90da663fa
davidbuttar <david.buttar@gmail.com>
2016-06-04 22:40:48 +0100

    boom

--- app/js/snapTest.js
+++ app/js/snapTest.js
@@ -12,16 +12,20 @@ bigCircle.attr({
 var degreeOffset45OnCircle = Math.sqrt(2)/2;
 var degreeOffset45OnCircleLeftOver = 1 - Math.sqrt(2)/2;
 
-function degreeArc45(radius, sweepFlag){
+function degreeArc45(radius, sweepFlag, reverse){
     sweepFlag = sweepFlag || 0;
     var y = degreeOffset45OnCircle * radius;
     var x = degreeOffset45OnCircleLeftOver * radius;
+    if(reverse){
+        y = -y;
+        x = -x;
+    }
     return 'a '+radius+' '+radius+' 0 0 '+sweepFlag+' '+x+' '+y;
 }
 
 //a 10 10 0 0 0 2.92 7.07
 
-var c = s.path("M10 10 l 30 30 l 0 30 l 100 0 a 10 10 90 0 1 10 10 l 0 100 "+degreeArc45(10)+" l 30 30 "+degreeArc45(10,1)+" l 0 20 a 10 10 90 0 0 10 10 l 20 0 a 10 10 90 0 0 10 -10 l 0 -100 l 5 0 l 0 100 a 15 15 0 0 1 -15 15 l -20 0" ).attr({
+var c = s.path("M 30 30 l 100 0 a 10 10 90 0 1 10 10 l 0 100 "+degreeArc45(10)+" l 30 30 "+degreeArc45(10,1)+" l 0 20 a 10 10 90 0 0 10 10 l 20 0 a 10 10 90 0 0 10 -10 l 0 -100 l 5 0 l 0 100 a 15 15 0 0 1 -15 15 l -20 0 a 15 15 0 0 1 -15 -15 l 0 -20"+degreeArc45(5,0,1)+" l -30 -30"+degreeArc45(15,1,1)+" l 0 -100 a 5 5 0 0 0 -5 -5 l -100 0" ).attr({
     fill: "none",
     stroke: "#bada55",
     strokeWidth: 1
