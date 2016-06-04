431fe58d4b6b3ec97f6060a0b6d3e80132bb5a28
davidbuttar <david.buttar@gmail.com>
2016-06-04 23:00:08 +0100

    super boom

--- app/js/snapTest.js
+++ app/js/snapTest.js
@@ -25,8 +25,8 @@ function degreeArc45(radius, sweepFlag, reverse){
 
 //a 10 10 0 0 0 2.92 7.07
 
-var c = s.path("M 30 30 l 100 0 a 10 10 90 0 1 10 10 l 0 100 "+degreeArc45(10)+" l 30 30 "+degreeArc45(10,1)+" l 0 20 a 10 10 90 0 0 10 10 l 20 0 a 10 10 90 0 0 10 -10 l 0 -100 l 5 0 l 0 100 a 15 15 0 0 1 -15 15 l -20 0 a 15 15 0 0 1 -15 -15 l 0 -20"+degreeArc45(5,0,1)+" l -30 -30"+degreeArc45(15,1,1)+" l 0 -100 a 5 5 0 0 0 -5 -5 l -100 0" ).attr({
+var c = s.path("M 20 20 l 100 0 a 10 10 90 0 1 10 10 l 0 100 "+degreeArc45(10)+" l 30 30 "+degreeArc45(10,1)+" l 0 20 a 10 10 90 0 0 10 10 l 20 0 a 10 10 90 0 0 10 -10 l 0 -100 l 5 0 l 0 100 a 15 15 0 0 1 -15 15 l -20 0 a 15 15 0 0 1 -15 -15 l 0 -20"+degreeArc45(5,0,1)+" l -30 -30"+degreeArc45(15,1,1)+" l 0 -100 a 5 5 0 0 0 -5 -5 l -100 0 Z" ).attr({
     fill: "none",
-    stroke: "#bada55",
+    stroke: "#ffa500",
     strokeWidth: 1
 });
